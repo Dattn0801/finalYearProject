7cdccd6485fae8f5082fc19596bc612b8f22b875
Dattn0801 <Dattn0801>
2022-03-22 18:18:47 +0700

    config nx, angular

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
+];
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ apps/ngshop/src/app/shared/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngteam-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
